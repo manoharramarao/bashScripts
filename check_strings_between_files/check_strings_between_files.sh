de143eea45e7013f957f02658887ae3d6e5c19a9
Manohar Ramarao <manoharramarao@gmail.com>
2016-12-03 01:30:36 +0100

    usage added

--- check_strings_between_files/check_strings_between_files.sh
+++ check_strings_between_files/check_strings_between_files.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+#-------------------------------------------------------------------------------
+# Usage:
+# reads line from file jars_inside_hybris
+# splits the line by "/" and considers the last string as pattern
+# searches pattern in jars_inside_custom
+# if not present it prints the line
+#-------------------------------------------------------------------------------
+
 while read -r line
 do
   jar_name="$(echo $line | rev | cut -d '/' -f1 | rev)"
   #see if exists in the 2nd file
   if ! grep -q $jar_name jars_inside_hybris.log; then
     echo $line
-  fi    
-done < jars_inside_custom.log 
+  fi
+done < jars_inside_custom.log
